f769dc1f1e2e6868277fb9d6b6bd75f1c2c6d148
fankangsong <fankangsong@gmail.com>
2026-02-28 03:20:51 +0000

    update script

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ if [ -f .env ]; then
   export $(cat .env | xargs)
 fi
 
+echo 'start syncing strava data.'
+
 python run_page/strava_sync.py ${STRAVA_CLIENT_ID} ${STRAVA_CLIENT_SECRET} ${STRAVA_REFRESH_TOKEN} --only-run
 
 
